b5e6a1efd6bc1fd25124f4ef69e5f44c916f2f8c
BryceLindley <lindleybryce@gmail.com>
2020-04-07 21:39:47 -0700

    updated JsonUtils with KEYS and build.gradle

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -5,48 +5,59 @@ import com.udacity.sandwichclub.model.Sandwich;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
 
-    private static String PLACEOFORIGIN = "placeOfOrigin";
-    private static String DESCRIPTION = "description";
-    private static String IMAGE = "image";
-
     public static Sandwich parseSandwichJson(String json) {
 
+        final String KEY_NAME = "name";
+        final String KEY_MAIN_NAME = "mainName";
+        final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+        final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+        final String KEY_DESCRIPTION = "description";
+        final String KEY_INGREDIENTS = "ingredients";
+        final String KEY_IMAGE = "image";
+
+        Sandwich sandwich = null;
+
         try {
+
             //Serializing
-            JSONObject sandwichDetails = new JSONObject(json);
+            JSONObject parsingText = new JSONObject(json);
 
             // Convert Object Keys
-            JSONObject name = sandwichDetails.getJSONObject("name");
-            PLACEOFORIGIN = sandwichDetails.getString("placeOfOrigin");
-            DESCRIPTION = sandwichDetails.getString("description");
-            IMAGE = sandwichDetails.getString("image");
 
-            // Converting JSON to String
-            String mainName = name.getString("mainName");
-            JSONArray ingredients = sandwichDetails.getJSONArray("ingredients");
-            JSONArray alsoKnownAs = name.getJSONArray("alsoKnownAs");
+            JSONObject name = parsingText.getJSONObject(KEY_NAME);
+            String mainName = name.getString(KEY_MAIN_NAME);
 
-            // JSON Array to String
-            List<String> secondName = new ArrayList<>();
+            String placeOfOrigin = parsingText.getString(KEY_PLACE_OF_ORIGIN);
+            String description = parsingText.getString(KEY_DESCRIPTION);
+            String image = parsingText.getString(KEY_IMAGE);
+
+
+            // JSONArray alsoKnownAs to String
+            JSONArray alsoKnownAs = name.getJSONArray(KEY_ALSO_KNOWN_AS);
+            List<String> otherNames = new ArrayList<>();
             for (int i = 0; i < alsoKnownAs.length(); i++) {
-                secondName.add(alsoKnownAs.getString(i));
+                otherNames.add(alsoKnownAs.getString(i));
             }
 
+            // Converting JSONArray ingredients List to List[]
+            JSONArray ingredients = parsingText.getJSONArray(KEY_INGREDIENTS);
             List<String> ingredientsNeeded = new ArrayList<>();
             for (int i = 0; i < ingredients.length(); i++) {
                 ingredientsNeeded.add(ingredients.getString(i));
             }
 
-            return new Sandwich(mainName, secondName, PLACEOFORIGIN, DESCRIPTION, IMAGE, ingredientsNeeded);
+            return new Sandwich(mainName, otherNames, placeOfOrigin, description, image, ingredientsNeeded);
 
         } catch (JSONException exc) {
             exc.printStackTrace();
         }
+
         return null;
     }
 }
